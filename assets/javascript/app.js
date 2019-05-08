c65eada005889cdcfa50d72d86054fe4458e405b
Camerone Stoney <Cameronestoney@yahoo.com>
2019-05-08 13:52:34 -0700

    js and html changed for easier project

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,15 @@
 // list of objects
 var Q1=new Object;
-question= "The beaver is the national emblem of which country?";
-corAns = "Canada";
-wngAns1 = "Alaska";
-wngAns2 = "Germany";
-wngAns3 = "Norway";
+Q1.question= "The beaver is the national emblem of which country?";
+Q1.corAns = "Canada";
+Q1.wngAns1 = "Alaska";
+Q1.wngAns2 = "Germany";
+Q1.wngAns3 = "Norway";
+
+// place correct answers in an array
+// capture click
+// compare captured click with array of correct answers
+// if clicked answer is found, alert win
 
 var Q2= new Object;
 Q2.question = "Which singer’s real name is Stefani Joanne Angelina Germanotta?";
@@ -34,6 +39,11 @@ Q5.wngAns1 = "Bong";
 Q5.wngAns2 = "Ding";
 Q5.wngAns3 = "Geller";
 
+// array to check correct answer
+var arrayOfCorAns = [Q1.corAns, Q2.corAns, Q3.corAns, Q4.corAns, Q5.corAns]
+// console.log to test array
+console.log(arrayOfCorAns);
+
 // setting up the timer
 
 var number= 30;
@@ -53,3 +63,46 @@ function decrement(){
         // full page sorry you have lost. try the next question
     }
 }
+
+$( document ).ready(function() {
+// question 1
+    $("#question-1").html(Q1.question);
+    $("#answer-1").html(Q1.corAns);
+    $("#answer-2").html(Q1.wngAns1);
+    $("#answer-3").html(Q1.wngAns2);
+    $("#answer-4").html(Q1.wngAns3);
+
+// question 2
+
+$("#question-2").html(Q2.question);
+    $("#answer2-1").html(Q2.corAns);
+    $("#answer2-2").html(Q2.wngAns1);
+    $("#answer2-3").html(Q2.wngAns2);
+    $("#answer2-4").html(Q2.wngAns3);
+// question 3
+$("#question-3").html(Q3.question);
+    $("#answer3-1").html(Q3.corAns);
+    $("#answer3-2").html(Q3.wngAns1);
+    $("#answer3-3").html(Q3.wngAns2);
+    $("#answer3-4").html(Q3.wngAns3);
+
+// question 4
+$("#question-4").html(Q4.question);
+    $("#answer4-1").html(Q4.corAns);
+    $("#answer4-2").html(Q4.wngAns1);
+    $("#answer4-3").html(Q4.wngAns2);
+    $("#answer4-4").html(Q4.wngAns3);
+
+// question 5
+$("#question-5").html(Q5.question);
+    $("#answer5-1").html(Q5.corAns);
+    $("#answer5-2").html(Q5.wngAns1);
+    $("#answer5-3").html(Q5.wngAns2);
+    $("#answer5-4").html(Q5.wngAns3);
+
+
+});
+
+
+
+
